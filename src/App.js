430f0c5d2e2a9efa4b26fcca654de8e478c5115b
Dima Savkov <dimsaw85@gmail.com>
2022-04-08 00:30:28 +0300

    ready15

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import FeedbackOptions from './componets/FeedbackOptions';
 import Section from './componets/Section';
 import Statistics from './componets/Statictics';
 import Notification from './componets/Notification';
 
-class App extends Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    visible: false,
-  };
+export default function App () {
+ 
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [percent, setPercentage] = useState(0);
 
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
-    visible: this.props.visible,
-  };
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+    setPercentage(Math.ceil(
+      (100 /total()) * good,
+    ))
+    
+  }, [good, neutral, bad, total])
+
+ 
 
   handelIncrement = event => {
     this.setState(prevState => ({
@@ -40,34 +43,34 @@ class App extends Component {
     return percent;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  // render() {
+  //   const { good, neutral, bad } = this.state;
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handelIncrement}
-          />
-        </Section>
+  //   return (
+  //     <>
+  //       <Section title="Please leave feedback">
+  //         <FeedbackOptions
+  //           options={['good', 'neutral', 'bad']}
+  //           onLeaveFeedback={this.handelIncrement}
+  //         />
+  //       </Section>
 
-        <Section title="Statistics">
-          {this.state.visible ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </>
-    );
-  }
+  //       <Section title="Statistics">
+  //         {this.state.visible ? (
+  //           <Statistics
+  //             good={good}
+  //             neutral={neutral}
+  //             bad={bad}
+  //             total={this.countTotalFeedback()}
+  //             positivePercentage={this.countPositiveFeedbackPercentage()}
+  //           />
+  //         ) : (
+  //           <Notification message="There is no feedback"></Notification>
+  //         )}
+  //       </Section>
+  //     </>
+  //   );
+  // }
 }
 
 export default App;
